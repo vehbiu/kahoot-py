132be8351ff64efe7c8698e01907acfda53e8b67
Vehbi <126095007+vehbiu@users.noreply.github.com>
2024-10-19 11:04:13 -0500

    Don't use library to evaluate

--- kahoot/util/solver.py
+++ kahoot/util/solver.py
@@ -1,8 +1,4 @@
-from re import split
 from base64 import b64decode
-from py_mini_racer import MiniRacer
-
-_evaluator: MiniRacer = MiniRacer()
 
 def _do_xor(session_token: str, solution: str) -> str:
     decoded_token = b64decode(session_token).decode('utf-8', 'strict')
@@ -10,10 +6,18 @@ def _do_xor(session_token: str, solution: str) -> str:
     sess_chars = [ord(s) for s in decoded_token]
     return "".join([chr(sess_chars[i] ^ sol_chars[i % len(sol_chars)]) for i in range(len(sess_chars))])
 
+def decode(offset: int, message):
+    decoded_message = ''
+    
+    for position, char in enumerate(message):
+        decoded_char = chr((((ord(char) * position) + offset) % 77) + 48)
+        decoded_message += decoded_char
+    
+    return decoded_message
+
 def solve_challenge(session_token: str, text: str) -> str:
     text = text.replace('\t', '', -1).encode('ascii', 'ignore').decode('utf-8')
-    text = split("[{};]", text)
-    replace_function = "return message.replace(/./g, function(char, position) {"
-    rebuilt = [text[1] + "{", text[2] + ";", replace_function, text[7] + ";})};", text[0]]
-    solution = _evaluator.eval("".join(rebuilt))
+    offset: int = int(eval(text.split("offset = ")[1].split(";")[0]))
+    input = text.split("this, '")[1].split("'")[0]
+    solution = decode(offset, input)
     return _do_xor(session_token, solution)
